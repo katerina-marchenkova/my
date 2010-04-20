0eac70e830261930cee1ac5ceb3a1c58f29e6961
Oleg Shuruev <Oleg.Shuruev@cbs.com>
2010-04-20 20:05:57 +0400

    Added some tests for naming macros.

---
+++ StyleCop/Shuruev.StyleCop/Shuruev.StyleCop.Test/NamingMacroTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shuruev.StyleCop.CSharp;
+
+namespace Shuruev.StyleCop.Test
+{
+	/// <summary>
+	/// Testing naming macros work.
+	/// </summary>
+	[TestClass]
+	public class NamingMacroTest
+	{
+		/// <summary>
+		/// Tests engine for applying one or several rules.
+		/// </summary>
+		[TestMethod]
+		public void TestRulesApplying()
+		{
+			Regex regex;
+
+			regex = NamingMacro.BuildRegex("Oleg:Shuruev", String.Empty);
+			Assert.IsTrue(regex.IsMatch("Oleg"));
+			Assert.IsTrue(regex.IsMatch("Shuruev"));
+			Assert.IsFalse(regex.IsMatch("oleg"));
+			Assert.IsFalse(regex.IsMatch("shuruev"));
+			Assert.IsFalse(regex.IsMatch("olegus"));
+			Assert.IsFalse(regex.IsMatch("nonshuruev"));
+		}
+
+		/// <summary>
+		/// Tests $(AaBb) macro.
+		/// </summary>
+		[TestMethod]
+		public void TestMacroPascal()
+		{
+			Regex regex;
+
+			regex = NamingMacro.BuildRegex("$(AaBb)", String.Empty);
+			Assert.IsTrue(regex.IsMatch("Style"));
+			Assert.IsTrue(regex.IsMatch("StyleCop"));
+			Assert.IsFalse(regex.IsMatch("styleCop"));
+			Assert.IsFalse(regex.IsMatch("StyleCop+"));
+			Assert.IsFalse(regex.IsMatch("Style_Cop"));
+			Assert.IsFalse(regex.IsMatch("StyleC"));
+			Assert.IsFalse(regex.IsMatch("CSharpStyle"));
+
+			regex = NamingMacro.BuildRegex("$(AaBb)", "C");
+			Assert.IsTrue(regex.IsMatch("Style"));
+			Assert.IsTrue(regex.IsMatch("StyleCop"));
+			Assert.IsFalse(regex.IsMatch("styleCop"));
+			Assert.IsFalse(regex.IsMatch("StyleCop+"));
+			Assert.IsFalse(regex.IsMatch("Style_Cop"));
+			Assert.IsTrue(regex.IsMatch("StyleC"));
+			Assert.IsTrue(regex.IsMatch("CSharpStyle"));
+
+			regex = NamingMacro.BuildRegex("$(AaBb)", "C X");
+			Assert.IsTrue(regex.IsMatch("Style"));
+			Assert.IsTrue(regex.IsMatch("StyleCop"));
+			Assert.IsTrue(regex.IsMatch("StyleCX"));
+			Assert.IsTrue(regex.IsMatch("CXSharpStyle"));
+		}
+	}
+}
