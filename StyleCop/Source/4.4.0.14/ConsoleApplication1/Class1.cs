671ce2c5eb4e8be3cd6d7a3fa3452628bb1d0225
Oleg Shuruev <Oleg.Shuruev@cbs.com>
2010-09-02 15:56:41 +0400

    Added source code for 4.4.0.14.

---
+++ StyleCop/Source/4.4.0.14/ConsoleApplication1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+	public class Class1
+	{
+		public void Test()
+		{
+			while ((2 + 5 > 0))
+			{
+				int a = (10 + 20);
+			}
+
+			/*if ((2 + 5 > 0))
+			{
+				int a = 10;
+			}*/
+			// int a = (5 + 6);
+
+			// switch ((4)) { }
+
+			// long l1 = (long)checked(0.1f / 0.2f);
+
+			//long l2 = (long)checked((0.1f / 0.2f));
+
+			// long l3 = (long)checked(((0.1f / 0.2f)));
+		}
+	}
+}
---
+++ StyleCop/Source/4.4.0.14/ConsoleApplication1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.StyleCop;
+
+namespace ConsoleApplication1
+{
+	/// <summary>
+	/// Simple example for running StyleCop environment.
+	/// </summary>
+	public class Program
+	{
+		/// <summary>
+		/// Main program entry.
+		/// </summary>
+		public static void Main(string[] args)
+		{
+			string projectPath = @"C:\Users\Public\GIT\My\StyleCop\Source\4.4.0.14\ConsoleApplication1";
+			string filePath = @"C:\Users\Public\GIT\My\StyleCop\Source\4.4.0.14\ConsoleApplication1\Class1.cs";
+
+			StyleCopConsole console = new StyleCopConsole(null, false, null, null, true);
+			CodeProject project = new CodeProject(0, projectPath, new Configuration(null));
+
+			console.Core.Environment.AddSourceCode(project, filePath, null);
+
+			console.OutputGenerated += OnOutputGenerated;
+			console.ViolationEncountered += OnViolationEncountered;
+			console.Start(new[] { project }, true);
+			console.OutputGenerated -= OnOutputGenerated;
+			console.ViolationEncountered -= OnViolationEncountered;
+			console.Dispose();
+
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+
+		/// <summary>
+		/// Handles generated output.
+		/// </summary>
+		private static void OnOutputGenerated(object sender, OutputEventArgs e)
+		{
+			Console.WriteLine(e.Output);
+		}
+
+		/// <summary>
+		/// Handles encountered violations.
+		/// </summary>
+		private static void OnViolationEncountered(object sender, ViolationEventArgs e)
+		{
+			Console.WriteLine("{0}: {1}", e.Violation.Rule.CheckId, e.Message);
+		}
+	}
+}
